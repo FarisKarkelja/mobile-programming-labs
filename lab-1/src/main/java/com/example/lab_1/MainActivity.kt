363665a6d225e6608ba7711c832b5c7d0b2221d9
Faris Karkelja <faree5803@gmail.com>
2026-03-06 20:30:43 +0100

    lab-1 task

--- lab-1/src/main/java/com/example/lab_1/MainActivity.kt
+++ lab-1/src/main/java/com/example/lab_1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lab_1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,18 +13,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.lab_1.ui.theme.MobilelabsTheme
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("Startup Tag", "Startup Message")
         enableEdgeToEdge()
         setContent {
             MobilelabsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    WelcomeScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -33,7 +39,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Welcome to $name!",
         modifier = modifier
     )
 }
@@ -42,6 +48,27 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MobilelabsTheme {
-        Greeting("Android")
+        Greeting("Mobile Programming!")
+    }
+}
+
+@Composable
+fun WelcomeScreen(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+
+    Column(modifier = modifier) {
+        Text(text = "Welcome to Mobile Programming!")
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(onClick = {
+            Log.i("Button tag", "Button message")
+            Log.e("Error tag", "Error message")
+            Toast.makeText(
+                context,
+                "Button clicked!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }) {
+            Text("Click me")
+        }
     }
 }
